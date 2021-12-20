54e3b98a2ccf97ab41390458336df5d29a10eb2c
Yage Zhao <yz128@rice.edu>
2021-12-19 22:52:43 -0600

    n

--- test.cpp
+++ test.cpp
@@ -58,7 +58,7 @@ int main() {
 
     int MAX_ITERATION_DDA = 100000;
     double MAX_ERROR = 0.00001;
-    int MAX_ITERATION_EVO = 20;
+    int MAX_ITERATION_EVO = 10;
 
     list<string> ObjectFunctionNames{ "IntegratedE" };
 
