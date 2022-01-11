61437cdb5c56e1f1674e9f1edc4244563a28e14f
Yage Zhao <yz128@rice.edu>
2022-01-11 10:59:43 -0600

    n

--- test.cpp
+++ test.cpp
@@ -4,41 +4,39 @@
 
 
 int main() {
-     
-    
-    string save_position = "./200nm2-sub100nm-Air-TiN-Ti-PointE7-lam500-period-ones-r_filter2-beta80/";       //output file
+
+    string save_position = "./thick400-diel2d5-phi0theta0-lam500-size2000-focus450-2D/";       //output file
     Vector3d l;
     Vector3d center;
-    l << 19.0, 19.0, 29.0;    //Size of the initialization block. 81*81*17 pixels in total.
+    l << 80.0, 80.0, 16.0;    //Size of the initialization block. 81*81*17 pixels in total.
     center << l(0) / 2, l(1) / 2, l(2) / 2;      //Center of the block.
     int Nx, Ny, Nz;
     Nx = round(l(0) + 3); Ny = round(l(1) + 3); Nz = round(l(2) + 1);   //Size of the design space. Notice that this sets the limits for coordinates, 
                                                                         //but actual pixels outside the 81*81*7 are not included in simulation. 
                                                                         //However, if geometry has pixels outside this space, they will be cut off.
-
+    cout << center << endl;
     int N = 0;                                                          //N counts the number of pixels in the geometries simulated. Initail is one.
-
+    Vector3i bind(1, 1, 17);                                             //binding in x,y,z. 2 means every 2 pixels will have the same material index. 3 means every 3.
                                                                         //This can be used to control the finest feature size of the designed structure.
-    double d = 10;                                                      //Size of pixel. Here 25nm.   
+    double d = 25;                                                      //Size of pixel. Here 25nm.   
     double E0 = 1.0;                                                    //Input field amplitude. 1V/m.
-    double epsilon = 100;                                                //Fixed learning rate of the optimization.
-    //double focus = (l(2) + 2) * d;   //nm                               //Focal spot is 50nm higher than the upper boundary of the intialization block.
-    //cout << focus << endl;
-    int MAX_ITERATION_DDA = 100000;              
-    //Number of maximum DDA iterations.
+    double epsilon = 10;                                                //Fixed learning rate of the optimization.
+    double focus = (l(2) + 2) * d;   //nm                               //Focal spot is 50nm higher than the upper boundary of the intialization block.
+    cout << focus << endl;
+    int MAX_ITERATION_DDA = 100000;                                     //Number of maximum DDA iterations.
     double MAX_ERROR = 0.00001;                                         //Maximum error of DDA.
-    int MAX_ITERATION_EVO = 100;                                        //Number of topology optimization.
-
+    int MAX_ITERATION_EVO = 200;                                        //Number of topology optimization.
+    list<double> ObjectParameter{ center(0) * d,center(1) * d,focus };  //Focal spot postition.
     bool HavePathRecord = false;
     bool HavePenalty = false;
-    bool HaveOriginHeritage = false;
+    bool HaveOriginHeritage = true;
     bool HaveAdjointHeritage = false;
     double PenaltyFactor = 0.0001;
     Vector3d n_K;                                                       //Wave vector direction.
     Vector3d n_E0;                                                      //Electric field polarization direction.
     int theta_num = 1;                                                  //Number of theta
     VectorXd theta(theta_num);
-    theta << 180;                                                         //Theta values.
+    theta << 0;                                                         //Theta values.
     int phi_num = 1;                                                    //Number of phi
     VectorXd phi(phi_num);
     phi << 0;                                                           //Phi values. For details of how theta and phi are defined, read tutorial.
@@ -46,7 +44,7 @@ int main() {
     VectorXd lam(lam_num);
     lam << 500;
     VectorXcd material;
-    list<string> mat_l{ "Air", "TiN", "Ti" };
+    list<string> mat_l{ "Air", "2.5" };
     material = Get_X_material(mat_l, lam(0), "nm");              //Air as substrate. material with permittivity of 2.5 as design material.
 
 
@@ -57,38 +55,18 @@ int main() {
     list<Structure> ln;
     Space S(&total_space, Nx, Ny, Nz, N, &ln);
     Vector3i direction;
-
-    Vector3d l1, l2, l3, center1, center2, center3;
-    l1 << 19.0, 19.0, 11.0;
-    center1 << l1(0) / 2, l1(1) / 2, 23.5;
-    l2 << 19.0, 19.0, 7.0;
-    center2 << l2(0) / 2, l2(1) / 2, 13.5;
-    l3 << 19.0, 19.0, 9.0;
-    center3 << l3(0) / 2, l3(1) / 2, 4.5;
-
-    Structure s1(S.get_total_space(), l1, center1);                       //Initialize the block which is 80*80*16 in terms of intervals or 81*81*17 in terms of pixels.
-    Structure s2(S.get_total_space(), l2, center2);
-    Structure s3(S.get_total_space(), l3, center3);
+    Structure s1(S.get_total_space(), l, center);                       //Initialize the block which is 80*80*16 in terms of intervals or 81*81*17 in terms of pixels.
     S = S + s1;                                                         //Add the geometry into the space.
-    S = S + s2;
-    S = S + s3;
-
-    Vector3i bind(1, 1, 12);                                             //binding in x,y,z. 2 means every 2 pixels will have the same material index. 3 means every 3.
-
-
     VectorXi* s1geometry = s1.get_geometry();
-    VectorXi* s2geometry = s2.get_geometry();
-    VectorXi* s3geometry = s3.get_geometry();
     list<VectorXi*> FPGeometryl{ s1geometry };
-    list<VectorXi*> BPGeometryl{ s2geometry, s3geometry };
-    list<double> BParal{ 1.0, 2.0 };
+    list<VectorXi*> BPGeometryl{};
+    list<double> BParal{};
     bool Filter = true;
     double r_f = 2.0;
     FilterOption filteropt(0.0, 80.0, 0.5, "piecewise", r_f);
-    SpacePara spacepara(&S, bind, "ONES", FPGeometryl, BPGeometryl, BParal, Filter, &filteropt);
-    //SpacePara is where the parameter<->geometry link is established.
+    SpacePara spacepara(&S, bind, "ONES", FPGeometryl, BPGeometryl, BParal, Filter, &filteropt);                              //Initialize with material index of 1, which is permittivity=2.5 in this case.
+                                                                        //SpacePara is where the parameter<->geometry link is established.
     list<string> ObjectFunctionNames{ "PointE" };                       //Name of the object function.
-    list<double> ObjectParameter{ center2(0) * d,center2(1) * d,17.0 * d };  //Focal spot postition.
     list<list<double>*> ObjectParameters{ &ObjectParameter };
     list<DDAModel> ModelList;
     list<DDAModel*> ModelpointerList;
@@ -97,13 +75,7 @@ int main() {
     CoreStructure CStr(&spacepara, d);                                 //Used as a interface to update parameters       
     list<AProductCore> CoreList;
     list<AProductCore*> CorePointList;
-    int m, n;
-    double Lm, Ln;
-    m = 50;
-    n = 50;
-    Lm = 20 * d;
-    Ln = 20 * d;
-    AProductCore Core1(&CStr, lam(0), material, m, n, Lm, Ln, "FCD");                //Matrix vector product is carried out in AProductCore class. So in this step, wavelength and actual permittivity comes in.
+    AProductCore Core1(&CStr, lam(0), material, "LDR");                //Matrix vector product is carried out in AProductCore class. So in this step, wavelength and actual permittivity comes in.
     CorePointList.push_back(&Core1);
     ofstream Common;
     Common.open(save_position + "Commondata.txt");
@@ -124,23 +96,39 @@ int main() {
                                                                       //And one AProductCore can be shared among several DDAModel, such that the memory consumption for multiple-angle optimization
                                                                       //should be similar to single-angle case. Of course, this is optimization for performance for a range of angles averaged. 
             for (int j = 0; j <= phi_num - 1; j++) {
-                double theta_tmp = theta(i) * M_PI / 180;
-                double phi_tmp = phi(j) * M_PI / 180;
-                n_K << sin(theta_tmp) * cos(phi_tmp), sin(theta_tmp)* sin(phi_tmp), cos(theta_tmp);
-                n_E0 = nEPerpinXZ(theta_tmp, phi_tmp);
-                if (CheckPerp(n_E0, n_K) == false) {
-                    cout << "----------------------------------------theta" << theta[i] << "phi" << phi[j] << "Not perpendicular---------------------------------------" << endl;
+                if (theta(i) != 0) {
+                    double theta_tmp = theta(i) * M_PI / 180;
+                    double phi_tmp = phi(j) * M_PI / 180;
+                    n_K << sin(theta_tmp) * cos(phi_tmp), sin(theta_tmp)* sin(phi_tmp), cos(theta_tmp);
+                    n_E0 = nEPerpinXZ(theta_tmp, phi_tmp);
+                    if (CheckPerp(n_E0, n_K) == false) {
+                        cout << "----------------------------------------theta" << theta[i] << "phi" << phi[j] << "Not perpendicular---------------------------------------" << endl;
+                    }
+                    if (k == 0) {
+                        AngleInfo << theta[i] << endl;
+                        AngleInfo << phi[j] << endl;
+                        nEInfo << n_E0(0) << " " << n_E0(1) << " " << n_E0(2) << endl;
+                    }
+                    DDAModel Model(Core, n_K, E0, n_E0);
+                    ModelList.push_back(Model);
                 }
-                if (k == 0) {
-                    AngleInfo << theta[i] << endl;
-                    AngleInfo << phi[j] << endl;
-                    nEInfo << n_E0(0) << " " << n_E0(1) << " " << n_E0(2) << endl;
-                }
-                DDAModel Model(Core, n_K, E0, n_E0);
-                ModelList.push_back(Model);
-
             }
         }
+        double theta_tmp = 0 * M_PI / 180;
+        double phi_tmp = 0 * M_PI / 180;
+        n_K << sin(theta_tmp) * cos(phi_tmp), sin(theta_tmp)* sin(phi_tmp), cos(theta_tmp);
+        n_E0 = nEPerpinXZ(theta_tmp, phi_tmp);
+        if (CheckPerp(n_E0, n_K) == false) {
+            cout << "----------------------------------------theta" << 0 << "phi" << 0 << "Not perpendicular---------------------------------------" << endl;
+        }
+        if (k == 0) {
+            AngleInfo << 0.0 << endl;
+            AngleInfo << 0.0 << endl;
+            nEInfo << n_E0(0) << " " << n_E0(1) << " " << n_E0(2) << endl;
+        }
+        DDAModel Model(Core, n_K, E0, n_E0);
+        ModelList.push_back(Model);
+
         it++;
     }
     AngleInfo.close();
